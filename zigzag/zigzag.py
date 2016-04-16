8e33d104502ebaeed90abd0b3fc2316541bf47ea
Administrator <Administrator@DESKTOP-CP5BUEN>
2016-04-16 12:07:42 +0800

    zigzag array

--- zigzag/zigzag.py
+++ zigzag/zigzag.py
@@ -3,4 +3,16 @@ Created on 2016 4 16
 
 @author: Administrator
 '''
-print 'x'
+def zigzagArray(array):
+    for i in range(len(array)-1):
+        if i % 2 == 0 and array[i] > array[i+1]:
+            tmp = array[i]
+            array[i] = array[i+1]
+            array[i+1] = tmp
+        if i % 2 == 1  and array[i] < array[i+1]:
+            tmp = array[i]
+            array[i] = array[i+1]
+            array[i+1] = tmp 
+    return array
+
+print (zigzagArray([3,5,2,1,7,9]))
