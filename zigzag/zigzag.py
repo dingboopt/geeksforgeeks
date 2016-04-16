9c279bed62cad56d0b34d7104f12759c35433852
Administrator <Administrator@DESKTOP-CP5BUEN>
2016-04-16 13:25:18 +0800

    add check function

--- zigzag/zigzag.py
+++ zigzag/zigzag.py
@@ -15,4 +15,18 @@ def zigzagArray(array):
             array[i+1] = tmp 
     return array
 
-print (zigzagArray([3,5,2,1,7,9]))
+def check(array):
+    print 'array is ', array
+    for i in range(len(array)-1):
+        if i % 2 == 0 and array[i] > array[i+1]:
+            return False
+        if i % 2 == 1  and array[i] < array[i+1]:
+            return False
+        
+        return True
+    
+from random import randint
+
+x=[randint(0,9) for p in range(randint(0,9))]
+    
+print (check(zigzagArray(x)))
